9472b07818114e3c4da5c1f8158c69210f049757
Andrew Clarke <andrewclarke.aron@gmail.com>
2022-08-16 17:58:13 +1000

    This is a working state

--- src/views/auth.py
+++ src/views/auth.py
@@ -7,13 +7,11 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBasic()
 
-
-def verify_hash(plain_password, hashed_password):
+def verify_hash(plain_password:str, hashed_password:str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_hash(password):
+def create_hash(password:str) -> str:
     return pwd_context.hash(password)
-
 
 def login(credentials: HTTPBasicCredentials = Depends(security)):
     # correct_username = secrets.compare_digest(credentials.username, "admin")
